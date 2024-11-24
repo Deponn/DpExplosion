16c505e2bb57ebb8e570ba1b56d61b3f1615335a
Deponn <Deponn.50nin@outlook.jp>
2024-11-24 18:28:32 +0900

    first

---
+++ src/main/java/deponn50nin/dpexplosion/DpExplosion.java
@@ -0,0 +1,17 @@
+package deponn50nin.dpexplosion;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DpExplosion extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
